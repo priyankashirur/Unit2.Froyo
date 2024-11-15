8c8be8f3ae02c978eee182a92186550eaad21336
Priyanka Shirur <pshirur323@gmail.com>
2024-11-15 08:36:27 -0800

    put into a function

--- index.js
+++ index.js
@@ -14,21 +14,23 @@ console.log("Array of user input: ", flavorsarray);
 
 //parse through the array variable and for each - add it to an object with key of the array entry and count of how many times it appears.
 
-//create object we will be filling
-let flavorCounts = {};
-
-//loop through each flavor. for each flavor in the flavor array, if the key in the object exists increase the value by 1. if the key in the object does not exist, set it as 1.
-for (let flavor of flavorsarray) {
-  console.log("Each flavor in array is: ", flavor);
-  //checking if the key exists
-  if (flavor in flavorCounts) {
-    flavorCounts[flavor]++;
-  }
-  // if the key doesnt exist
-  else {
-    flavorCounts[flavor] = 1;
-  }
-}
-
 //print out the object
-console.table("The final flavor counts are: ", flavorCounts);
+console.table(getCounts(flavorsarray));
+
+function getCounts(flavorsarray) {
+  let flavorCounts = {};
+
+  //loop through each flavor. for each flavor in the flavor array, if the key in the object exists increase the value by 1. if the key in the object does not exist, set it as 1.
+  for (let flavor of flavorsarray) {
+    console.log("Each flavor in array is: ", flavor);
+    //checking if the key exists
+    if (flavor in flavorCounts) {
+      flavorCounts[flavor]++;
+    }
+    // if the key doesnt exist
+    else {
+      flavorCounts[flavor] = 1;
+    }
+  }
+  return flavorCounts;
+}
